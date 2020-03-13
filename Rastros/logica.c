5c59595264483083544040f516417847f34b93e4
Rui Jordão Sampaio Gonçalves <58782713+jordan21pt@users.noreply.github.com>
2020-03-13 02:43:55 +0000

    Add files via upload

--- Rastros/logica.c
+++ Rastros/logica.c
@@ -7,5 +7,31 @@
 
 int jogar(ESTADO *e, COORDENADA c)
 {
+    //esta função deverá receber o estado atual e uma coordenada e modificar
+    //o estado ao jogar na casa correta se a jogada for válida. A função devolve
+    //verdadeiro (valor diferente de zero) se for possível jogar e falso (zero) caso
+    //não seja possível.
+
+    printf("jogar %d %d\n", c.coluna, c.linha);
+    e->tab[8-c.linha][c.coluna-1] = BRANCA;
+    e->tab[e->ultima_jogada.linha][e->ultima_jogada.coluna] = PRETA;
+
+    if (e->jogador_atual == 1)
+    {
+        e->jogadas[e->num_jogadas].jogador1.coluna = c.coluna;
+        e->jogadas[e->num_jogadas].jogador1.linha = c.linha;
+    }
+
+    else
+    {
+        e->jogadas[e->num_jogadas].jogador2.coluna = c.coluna;
+        e->jogadas[e->num_jogadas].jogador2.linha = c.linha;
+        e->num_jogadas++;
+    }
+
+    e->jogador_atual = (e->jogador_atual == 1)? 2 : 1;
+    e->ultima_jogada.linha = c.linha;
+    e->ultima_jogada.coluna= c.coluna;
+    return 1;
 
 }
