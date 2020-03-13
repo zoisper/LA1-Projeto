d7223803200cc37b831cec309e627249fce5c862
Vicente de Carvalho Castro <61989504+elvicente13@users.noreply.github.com>
2020-03-13 15:31:37 +0000

    Add files via upload

--- Rastros/dados.c
+++ Rastros/dados.c
@@ -8,11 +8,20 @@ int i,j;
 ESTADO *inicializar_estado()
 {
     ESTADO *e = (ESTADO *) malloc(sizeof(ESTADO));
-
+    for(i=0;i<8;i++)
+        for(j=0;j<8;j++)
+            e->tab[i][j] = VAZIO;
+            e->tab[3][4] = BRANCA;
     e->jogador_atual = 1;
     e->num_jogadas = 0;
-
-
+    e->ultima_jogada.coluna = 4;
+    e->ultima_jogada.linha = 3;
+    for(i=0;i<32;i++){
+        e->jogadas[i].jogador1.coluna = 0;
+        e->jogadas[i].jogador1.linha = 0;
+        e->jogadas[i].jogador2.coluna = 0;
+        e->jogadas[i].jogador2.linha = 0;
+    }
 
     return e;
 }
