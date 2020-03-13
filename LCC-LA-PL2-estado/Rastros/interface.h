87d472d99f764474b752da6cf7d0b466e1b63fb0
Vicente de Carvalho Castro <61989504+elvicente13@users.noreply.github.com>
2020-03-13 14:31:52 +0000

    Add files via upload

---
+++ LCC-LA-PL2-estado/Rastros/interface.h
@@ -0,0 +1,12 @@
+//
+// Created by tiago on 08/03/20.
+//
+
+
+#ifndef PROJETO_INTERFACE_H
+#define PROJETO_INTERFACE_H
+#include "dados.h"
+
+void mostrar_tabuleiro(ESTADO e);
+
+#endif //PROJETO_INTERFACE_H
