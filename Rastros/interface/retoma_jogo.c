991729fab49e2b45dc050e9934fa2ce6e6403686
zoisper <zoisper@gmail.com>
2020-03-22 13:33:39 +0000

    retoma_jogada

--- Rastros/interface/retoma_jogo.c
+++ Rastros/interface/retoma_jogo.c
@@ -6,5 +6,13 @@
 
 void retoma_jogo (ESTADO *e, FILE *fp)
 {
-
+int i, j, jogador, jogadas;
+    char carater;
+    COORDENADA coord;
+    for (i=0; i<8; i++)
+        for(j=0;j<8;j++)
+        {
+            carater = fgetc(fp);
+            retoma_casa(e,i, j,carater);
+        
 }
